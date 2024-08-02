45343899b58fd5c9dafc93dca70d6f350164b94b
EvgenyGr <demorein6@gmail.com>
2024-08-02 11:36:25 +0500

    Add main()

---
+++ main.py
@@ -0,0 +1,17 @@
+#Импорты модулей
+from tkinter import ttk
+from tkinter import Tk
+from func import *
+
+#Основная функция
+def main():
+    root = Tk() #Root
+    root.geometry("750x450")
+    root.resizable(False, False)
+    
+    #Какой-то код
+    
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
